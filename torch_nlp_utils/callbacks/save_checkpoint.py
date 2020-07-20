2505ca5112685f92f934f9cb5ad793d93b41847f
nemexur <alex.milogradsky@gmail.com>
2020-07-20 12:26:09 +0300

    Updated SaveCheckpoint

--- torch_nlp_utils/callbacks/save_checkpoint.py
+++ torch_nlp_utils/callbacks/save_checkpoint.py
@@ -1,5 +1,8 @@
+from typing import Dict, Any
 import os
+import json
 import torch
+import shutil
 
 
 class SaveCheckpoint:
@@ -8,25 +11,43 @@ class SaveCheckpoint:
 
     Parameters
     ----------
+    model : `torch.nn.Module`, required
+        Torch model to monitor.
     directory : `str`, required
         Directory to save model checkpoints per epoch.
     keep_num_checkpoints : `int`, optional (default = `None`)
         Number of checkpoints to keep. If None then keep all of them.
     """
-    def __init__(self, directory: str, keep_num_checkpoints: int = None) -> None:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        directory: str,
+        keep_num_checkpoints: int = None,
+    ) -> None:
         os.makedirs(directory, exist_ok=False)
         if keep_num_checkpoints is not None and keep_num_checkpoints < 1:
             raise Exception('keep_num_checkpoints should be greater than 0')
         self._epoch_idx = 0
+        self._model = model
         self._directory = directory
         self._keep_num_checkpoints = keep_num_checkpoints
 
-    def __call__(self, model: torch.nn.Module) -> None:
+    def __call__(self, metrics: Dict[str, Any]) -> None:
         """Perform saving after one epoch."""
+        cur_epoch_dir = os.path.join(self._directory, f'epoch_{self._epoch_idx}')
+        os.makedirs(cur_epoch_dir, exist_ok=True)
+        # Save torch model
         torch.save(
-            model.state_dict(),
-            os.path.join(self._directory, f'epoch_{self._epoch_idx}.pt')
+            self._model.state_dict(),
+            os.path.join(cur_epoch_dir, f'epoch_{self._epoch_idx}.pt')
         )
+        # Save metrics
+        with open(
+            os.path.join(cur_epoch_dir, f'epoch_{self._epoch_idx}_metrics.json'),
+            'w',
+            encoding='utf-8'
+        ) as file:
+            json.dump(metrics, file, ensure_ascii=False, indent=2)
         self._epoch_idx += 1
         if self._keep_num_checkpoints:
             self._delete_spare_if_needed()
@@ -35,4 +56,4 @@ class SaveCheckpoint:
         checkpoints = os.listdir(self._directory)
         if len(checkpoints) > self._keep_num_checkpoints:
             for checkpoint in checkpoints[:-self._keep_num_checkpoints]:
-                os.remove(os.path.join(self._directory, checkpoint))
+                shutil.rmtree(os.path.join(self._directory, checkpoint), ignore_errors=True)
