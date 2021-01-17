f7e7d4714efe21754383df04ed0589b4b8d4d04e
nemexur <alex.milogradsky@gmail.com>
2021-01-17 22:23:39 +0300

    Added CollateBatch to data init.py

--- torch_nlp_utils/data/__init__.py
+++ torch_nlp_utils/data/__init__.py
@@ -1,3 +1,3 @@
 from .dataset_readers import DatasetReader
 from .vocabulary import Vocabulary, Namespace
-from .data_iterators import DataIterator, Batch
+from .data_iterators import DataIterator, Batch, CollateBatch
