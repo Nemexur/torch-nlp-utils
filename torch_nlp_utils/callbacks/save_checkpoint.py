96975d950b0e45a4762cf8a72adbadc53a47d26a
nemexur <alex.milogradsky@gmail.com>
2021-01-18 22:25:19 +0300

    SaveCheckpoint os.path -> pathlib.Path

--- torch_nlp_utils/callbacks/save_checkpoint.py
+++ torch_nlp_utils/callbacks/save_checkpoint.py
@@ -1,8 +1,8 @@
-from typing import Dict, Any
-import os
+from typing import Dict, Any, Union
 import json
 import torch
 import shutil
+from pathlib import Path
 from loguru import logger
 
 
@@ -14,7 +14,7 @@ class SaveCheckpoint:
     ----------
     model : `torch.nn.Module`, required
         Torch model to monitor.
-    directory : `str`, required
+    directory : `Union[str, Path]`, required
         Directory to save model checkpoints per epoch.
     keep_num_checkpoints : `int`, optional (default = `None`)
         Number of checkpoints to keep. If None then keep all of them.
@@ -22,7 +22,7 @@ class SaveCheckpoint:
 
     def __init__(
         self,
-        directory: str,
+        directory: Union[str, Path],
         model: torch.nn.Module = None,
         keep_num_checkpoints: int = None,
     ) -> None:
@@ -31,9 +31,11 @@ class SaveCheckpoint:
                 "Model is not passed in init. "
                 "Then you should pass dict to save with torch.save by yourself."
             )
-        os.makedirs(directory, exist_ok=False)
         if keep_num_checkpoints is not None and keep_num_checkpoints < 1:
             raise Exception("keep_num_checkpoints should be greater than 0")
+        self._directory = Path(directory)
+        # Create directory for checkpoint
+        self._directory.mkdir(exist_ok=False)
         self.epoch_idx = 0
         self._model = model
         self._directory = directory
@@ -53,21 +55,22 @@ class SaveCheckpoint:
         """Perform saving after one epoch."""
         if not save_dict and not self._model:
             raise Exception("You should pass save_dict on call if model is None.")
-        cur_epoch_dir = os.path.join(self._directory, f"epoch_{self.epoch_idx}")
-        os.makedirs(cur_epoch_dir, exist_ok=True)
+        cur_epoch_dir = self._directory / f"epoch_{self.epoch_idx}"
+        cur_epoch_dir.mkdir()
         # Save torch model
-        torch.save(save_dict or self._model.state_dict(), os.path.join(cur_epoch_dir, "model.pt"))
+        torch.save(save_dict or self._model.state_dict(), cur_epoch_dir / "model.pt")
         # Save metrics
-        with open(os.path.join(cur_epoch_dir, "metrics.json"), mode="w", encoding="utf-8") as file:
+        with (cur_epoch_dir / "metrics.json").open(mode="w", encoding="utf-8") as file:
             json.dump(metrics, file, ensure_ascii=False, indent=2)
         # Save best model
         if is_best_so_far:
+            # Save best model in parent directory
+            best_model_path = self._directory.parent / "best-model"
             logger.info(
-                f"Best validation performance so far. Copying to '{self._directory}/best'.",
+                f"Best validation performance so far. Copying to `{best_model_path}`.",
             )
-            best_model_path = os.path.join(self._directory, "best")
             # Delete directory if exists
-            if os.path.exists(best_model_path):
+            if best_model_path.exists():
                 shutil.rmtree(best_model_path)
             shutil.copytree(cur_epoch_dir, best_model_path)
             self._best_model_path = best_model_path
@@ -78,7 +81,10 @@ class SaveCheckpoint:
         self.epoch_idx += 1
 
     def _delete_spare_if_needed(self):
-        checkpoints = sorted(os.listdir(self._directory))
+        checkpoints = sorted(
+            [str(x) for x in self._directory.glob("epoch_*")],
+            key=lambda x: (len(x), x),
+        )
         if len(checkpoints) > self._keep_num_checkpoints:
             for checkpoint in checkpoints[:-self._keep_num_checkpoints]:
-                shutil.rmtree(os.path.join(self._directory, checkpoint))
+                shutil.rmtree(self._directory / checkpoint)
